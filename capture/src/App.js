38cd495e008bb38a14f2eb09173049eb42500d10
jidbujawe <76420686+jidbujawe@users.noreply.github.com>
2021-02-09 12:08:47 +1100

    Initial Setup

---
+++ capture/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Film</h1>
+      <h2>I really don't know what that was!</h2>
+    </div>
+  );
+}
+
+export default App;
