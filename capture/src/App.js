135dd4b1a0d296d775bedae3d0c74d91c3b96dd4
jidbujawe <76420686+jidbujawe@users.noreply.github.com>
2021-02-09 12:46:25 +1100

    Services Section

--- capture/src/App.js
+++ capture/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import AboutSection from "./components/AboutSection";
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-      <AboutSection />
+      <AboutUs />
     </div>
   );
 }
