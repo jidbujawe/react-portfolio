3c2c90c75568ba1c8b2322a4ddfb6fcfaf60c6fe
jidbujawe <76420686+jidbujawe@users.noreply.github.com>
2021-02-10 12:07:41 +1100

    Intro to Styled Components

--- capture/src/App.js
+++ capture/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import GlobalStyle from "./components/GlobalStyled";
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- capture/src/components/AboutSection.js
+++ capture/src/components/AboutSection.js
@@ -1,32 +1,68 @@
 import React from "react";
 import home1 from "../img/home1.png";
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div className="description">
-      <div className="title">
-        <div className="hide">
-          <h2>We work to make</h2>
+    <AboutStyled>
+      <DescriptionStyled className="description">
+        <div className="title">
+          <HideStyled>
+            <h2>We work to make</h2>
+          </HideStyled>
+          <HideStyled>
+            <h2>
+              your <span>dreams</span>
+            </h2>
+          </HideStyled>
+          <HideStyled>
+            <h2>come true.</h2>
+          </HideStyled>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, a
+            est quo voluptas id magnam accusantium ipsam exercitationem ab
+            eligendi?
+          </p>
+          <button>Contact us</button>
         </div>
-        <div className="hide">
-          <h2>
-            your <span>dreams</span>
-          </h2>
-        </div>
-        <div className="hide">
-          <h2>come true.</h2>
-        </div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, a est
-          quo voluptas id magnam accusantium ipsam exercitationem ab eligendi?
-        </p>
-        <button>Contact us</button>
-      </div>
-      <div className="image">
+      </DescriptionStyled>
+      <ImageStyled>
         <img src={home1} alt="guy with a camera" />
-      </div>
-    </div>
+      </ImageStyled>
+    </AboutStyled>
   );
 };
+
+// Styled components
+const AboutStyled = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const DescriptionStyled = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const ImageStyled = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const HideStyled = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
